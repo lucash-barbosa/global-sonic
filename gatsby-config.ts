6932aa0960f88ee3cc30f0403c84caf7551a0128
Lucash <lucas565lkj@gmail.com>
2023-01-03 18:22:38 -0300

    feat: add initial settings
    add commitzen; set gatsby to import images; add types png and svg to typescript and add dependencies

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,26 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Global Sonic`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'Global Sonic',
+    siteUrl: 'https://www.yourdomain.tld'
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    'gatsby-plugin-sharp', 
+    'gatsby-transformer-sharp', 
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+  ]
 };
 
 export default config;
