013d22b2047973a881064ee1f8414883c9d78bc6
Lucash <lucas565lkj@gmail.com>
2023-01-06 15:18:58 -0300

    fix: remove unnecessary plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,13 +13,6 @@ const config: GatsbyConfig = {
     'gatsby-plugin-sharp', 
     'gatsby-transformer-sharp', 
     'gatsby-plugin-styled-components',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/assets/`,
-      },
-    },
   ]
 };
 
